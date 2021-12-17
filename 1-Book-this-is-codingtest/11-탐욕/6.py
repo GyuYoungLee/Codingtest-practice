07d62a696fb236dc3eef9102c379e1551b85d7be
gyuyoung <ifos2u@naver.com>
2021-12-17 16:04:19 +0900

    book greedy

--- 1-Book-this-is-codingtest/11-탐욕/6.py
+++ 1-Book-this-is-codingtest/11-탐욕/6.py
@@ -4,32 +4,25 @@ import heapq
 
 
 def solution(food_times, k):
-    # 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
     if sum(food_times) <= k:
         return -1
 
-    # 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
-    q = []
-    for i in range(len(food_times)):
-        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
-        heapq.heappush(q, (food_times[i], i + 1))
-    print(q)
+    qu = []
+    for i, v in enumerate(food_times):
+        heapq.heappush(qu, [v, i + 1])  # 음식의 갯수, 번호
 
-    sum_value = 0  # 먹기 위해 사용한 시간
-    previous = 0  # 직전에 다 먹은 음식 시간
-    length = len(food_times)  # 남은 음식의 개수
+    n = len(qu)
+    pre = 0
 
-    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
-    while sum_value + ((q[0][0] - previous) * length) <= k:
-        now = heapq.heappop(q)[0]
-        sum_value += (now - previous) * length
-        length -= 1  # 다 먹은 음식 제외
-        previous = now  # 이전 음식 시간 재설정
-        print(q)
+    while (qu[0][0] - pre) * n <= k:
+        now = heapq.heappop(qu)[0]
+        k -= (now - pre) * n
 
-    # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
-    result = sorted(q, key=lambda x: x[1])  # 음식의 번호 기준으로 정렬
-    return result[(k - sum_value) % length][1]
+        n -= 1
+        pre = now
+
+    remain = sorted(qu, key=lambda x: x[1])
+    return remain[k % n][1]
 
 
 print(solution([3, 1, 2], 5))
